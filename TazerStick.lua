b152282d6ef4672ee9f563ee5725bce27fc3493e
DontNeg <lukianmokahel123@gmail.com>
2024-06-21 14:12:52 -0700

    Update TazerStick.lua

--- TazerStick.lua
+++ TazerStick.lua
@@ -1,32 +1,115 @@
-local InputService = game:GetService("UserInputService")
-local CollectionService = game:GetService("CollectionService")
-local tazerStick = script.Parent
-local tazerSound = tazerStick:WaitForChild("Sound")
-local camera = workspace.Camera
-local tazerMeshPart = camera:WaitForChild("viewModelTazerStick"):WaitForChild("TazerStick"):WaitForChild("MeshPart")
-local active = false
-local equipped = false
+local pathFindingService = game:GetService("PathfindingService")
+local runService = game:GetService("RunService")
+local enemy = script.Parent
+local humanoid = enemy:WaitForChild("Humanoid")
+local humanoidRootPart = enemy:WaitForChild("HumanoidRootPart")
+local rayParams = RaycastParams.new()
+local waypoints = workspace.Waypoints:GetChildren()
+local lastPos
+local animPlaying = false
+local RANGE = 60
+local DAMAGE = 30
+local AFTERSIGHT = 5
+local ATTACKDISTANCE = 5
 
-tazerStick.Equipped:Connect(function()
-	equipped = true
-end)
-tazerStick.Unequipped:Connect(function()
-	equipped = false
-	active = false
-	tazerSound:Stop()
-end)
+humanoidRootPart:SetNetworkOwner(nil)
+rayParams.FilterType = Enum.RaycastFilterType.Exclude
+rayParams.FilterDescendantsInstances = {enemy}
 
-InputService.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1 and equipped then
-		active = not active
-		if active then tazerSound:Play(); return end
-		tazerSound:Stop()
+local function checkJump(waypoint)
+	if waypoint.Action == Enum.PathWaypointAction.Jump then
+		humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
 	end
-end)
+end
+local function moveTo(destination)
+	humanoid:MoveTo(destination)
+	humanoid.MoveToFinished:Wait()
+end
 
-tazerMeshPart.Touched:Connect(function(part)
-	if CollectionService:HasTag(part,"Enemy") and part.Name ~= "HumanoidRootPart" or CollectionService:HasTag(part,"Enemy") and part.Name ~= "Humanoid" then
-		part:Destroy()
+local function pathFinding(destination)
+	local path = pathFindingService:CreatePath({AgentHeight = 5,AgentRadius = 3,AgentCanJump = true})
+	path:ComputeAsync(humanoidRootPart.Position, destination.Position)
+	if path.Status == Enum.PathStatus.Success then
+		for _, waypoint in pairs(path:GetWaypoints()) do
+			checkJump(waypoint)
+			path.Blocked:Connect(function()
+				path:Destroy()
+			end)
+			if animPlaying == false then
+				--walkAnim:Play()
+				animPlaying = true
+			end
+			--attackAnim:Stop()
+			local players = game.Players:GetPlayers()
+			local maxDistance = RANGE
+			local nearestTarget
+			local lastTarget
+			for _, player in pairs(players) do
+				local target = player.Character
+				if target then
+					local distance = (humanoidRootPart.Position - target.HumanoidRootPart.Position).Magnitude
+					local canSee = false
+					local direction = (target.HumanoidRootPart.Position - humanoidRootPart.Position).Unit
+					local ray = workspace:Raycast(humanoidRootPart.Position, direction * RANGE, rayParams)
+					if ray and ray.Instance then
+						if ray.Instance:IsDescendantOf(target) then
+							canSee = true
+						end
+					end
+					if distance < maxDistance then
+						lastTarget = target
+						if canSee then
+							nearestTarget = target
+							maxDistance = distance
+						end
+					end
+				end
+			end
+			if nearestTarget and nearestTarget.Humanoid.Health > 0 then
+				lastPos = nearestTarget.HumanoidRootPart.Position
+				local distance = (humanoidRootPart.Position - nearestTarget.HumanoidRootPart.Position).Magnitude
+				local canAttack = true
+				if distance > ATTACKDISTANCE then
+					checkJump(waypoint)
+					humanoid:MoveTo(nearestTarget.HumanoidRootPart.Position)
+				else
+					if canAttack then
+						canAttack = not canAttack
+						--npc.Head.AttackSound:Play()
+						--attackAnim:Play()
+						nearestTarget.Humanoid.Health -= DAMAGE
+						task.wait(0.5)
+						canAttack = not canAttack
+					end
+				end
+				break
+			else
+				if lastPos then
+					checkJump(waypoint)
+					moveTo(lastPos)
+					local pathInside = pathFindingService:CreatePath({AgentHeight = 5,AgentRadius = 3,AgentCanJump = true})
+					local count = 0
+					pathInside:ComputeAsync(humanoidRootPart.Position, lastTarget.HumanoidRootPart.Position)
+					for _, waypointInside in pairs(pathInside:GetWaypoints()) do
+						count = count + 1
+						if count == AFTERSIGHT then
+							break
+						end
+						checkJump(waypointInside)
+						moveTo(waypointInside.Position)
+					end
+					lastPos = nil
+					break
+				else
+					moveTo(waypoint.Position)
+				end
+			end
+		end
+	else
+		return
 	end
-end)
+end
 
+while task.wait(0.2) do
+	pathFinding(waypoints[math.random(#waypoints)])
+end
