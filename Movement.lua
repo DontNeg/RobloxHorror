94652500b7d5d46bc90704cdb8b7a62467d76cb7
DontNeg <lukianmokahel123@gmail.com>
2024-06-21 14:12:49 -0700

    Update

--- Movement.lua
+++ Movement.lua
@@ -9,8 +9,10 @@ local stamina = maxStamina
 local timer = 0
 local timerGoal = 3
 local moveSpeeds = {8 ,12, 18}
-local fovs = {70, 80}
-
+local fovs = {60, 70, 80}
+local movementKeys = {Enum.KeyCode.W, Enum.KeyCode.A, Enum.KeyCode.S, Enum.KeyCode.D}
+local moving = false
+local jumping = false
 
 function resetTimer()
 	if stamina > 0 then
@@ -19,55 +21,65 @@ function resetTimer()
 end
 
 InputService.InputBegan:Connect(function(key)
-	local movementKey = 
-			key.KeyCode == Enum.KeyCode.W 
-		or 	key.KeyCode == Enum.KeyCode.A 
-		or 	key.KeyCode == Enum.KeyCode.S 
-		or 	key.KeyCode == Enum.KeyCode.D
-	local moving = (player.RootPart.Velocity * Vector3.new(1,0,1)).Magnitude >= 1
-	if movementKey then
-		if _G.moveMode == "semiSprinting" then
-			resetTimer()
-			setWalkMode("sprinting")
-		elseif _G.moveMode == "semiCrouching" then
-			resetTimer()
-			setWalkMode("crouching")
+	if stamina > 0 then
+		if table.find(movementKeys, key.KeyCode) then  
+			if _G.moveMode == "semiSprinting" then
+				setWalkMode("sprinting")
+			elseif _G.moveMode == "semiCrouching" then
+				setWalkMode("crouching")
+			end
+		elseif key.KeyCode == Enum.KeyCode.LeftShift then
+			if moving then
+				setWalkMode("sprinting")
+			else
+				setWalkMode("semiSprinting")
+			end
+		elseif key.KeyCode == Enum.KeyCode.LeftControl then
+			if moving then
+				setWalkMode("crouching")
+			else
+				setWalkMode("semiCrouching")
+			end
 		end
-	elseif key.KeyCode == Enum.KeyCode.LeftShift then
-		if moving then
-			resetTimer()
-			setWalkMode("sprinting")
-		else
-			setWalkMode("semiSprinting")
-		end
-	elseif key.KeyCode == Enum.KeyCode.LeftControl then
-		if moving then
-			resetTimer()
-			setWalkMode("crouching")
-		else
-			setWalkMode("semiCrouching")
-		end
-	elseif key.KeyCode == Enum.KeyCode.Space and stamina>=5 then
+	end
+	if key.KeyCode == Enum.KeyCode.Space and stamina>=5 then
 		stamina-=5
 		timer = 0
 	end
 end)
 
 InputService.InputEnded:Connect(function(key)
-	local movementKey = 
-			key.KeyCode == Enum.KeyCode.W 
-		or 	key.KeyCode == Enum.KeyCode.A 
-		or 	key.KeyCode == Enum.KeyCode.S 
-		or 	key.KeyCode == Enum.KeyCode.D
 	if key.KeyCode == Enum.KeyCode.LeftShift or key.KeyCode == Enum.KeyCode.LeftControl then
 		setWalkMode("walking")
 	end
 end)
 
+RunService.PreAnimation:Connect(function(deltaTime)
+	local changeValue = 10 * deltaTime * 5
+	if _G.moveMode == "sprinting" then
+		stamina = math.max(0, stamina - 1 * deltaTime)
+		cam.FieldOfView = math.min(fovs[3], cam.FieldOfView + changeValue)
+	elseif _G.moveMode == "crouching" then
+		stamina = math.max(0, stamina - 0.5 * deltaTime)
+		cam.FieldOfView = math.max(fovs[1], cam.FieldOfView - changeValue)
+	elseif _G.moveMode == "walking" or string.find(_G.moveMode, "semi") ~= nil then
+		if timer >= 3 then
+			stamina = math.min(maxStamina, stamina + 2 * (deltaTime))
+		end
+		if cam.FieldOfView > fovs[2] then
+			cam.FieldOfView = math.max(fovs[2], cam.FieldOfView - changeValue)
+		else
+			cam.FieldOfView = math.min(fovs[2], cam.FieldOfView + changeValue)
+		end
+	end
+end)
+
 RunService.Heartbeat:Connect(function(deltaTime)
-	if (player.RootPart.Velocity * Vector3.new(0,1,0)).Magnitude >= 1 then
+	moving = (player.RootPart.Velocity * Vector3.new(1,0,1)).Magnitude >= 1
+	jumping = (player.RootPart.Velocity * Vector3.new(0,1,0)).Magnitude >= 1
+	if jumping then
 		setWalkMode("jumping")
-	elseif (player.RootPart.Velocity * Vector3.new(0,1,0)).Magnitude < 1 and not (_G.moveMode == "crouching") and not (_G.moveMode == "semiCrouching") and not (_G.moveMode == "sprinting") and not (_G.moveMode == "semiSprinting")  then
+	elseif not jumping and _G.moveMode == "jumping" then
 		setWalkMode("walking")
 	end
 	if stamina < 5 then
@@ -75,7 +87,6 @@ RunService.Heartbeat:Connect(function(deltaTime)
 	else
 		player:SetStateEnabled(Enum.HumanoidStateType.Jumping, true)
 	end
-	local moving = (player.RootPart.Velocity * Vector3.new(1,0,1)).Magnitude >= 1
 	if _G.moveMode == "sprinting" and not moving then
 		setWalkMode("semiSprinting")
 	elseif _G.moveMode == "crouching" and not moving then
@@ -84,35 +95,24 @@ RunService.Heartbeat:Connect(function(deltaTime)
 	if stamina <= 0 then
 		setWalkMode("walking")
 	end
-	local velocity = player.RootPart.Velocity
-	local changeValue = 10 * deltaTime * 5
-	if (_G.moveMode == "sprinting") or (_G.moveMode == "crouching") then
-		stamina = math.max(0, stamina - 1 * deltaTime)
-		if cam.FieldOfView<fovs[2] then
-			cam.FieldOfView = cam.FieldOfView + changeValue
-		end
-	elseif (not (_G.moveMode == "sprinting") and not (_G.moveMode == "crouching")) and not (_G.moveMode == "jumpingw") or stamina<=0 then
-		if cam.FieldOfView>fovs[1] then
-			cam.FieldOfView = cam.FieldOfView - changeValue
-		end
+	if _G.moveMode == "walking" or string.find(_G.moveMode, "semi") ~= nil then
 		if stamina<maxStamina and timer<3 then
 			timer = timer + (1*deltaTime)
 		end
-		if timer >= timerGoal then
-			stamina = math.min(maxStamina, stamina + 2 * (deltaTime))
-			if stamina == maxStamina then
-				timer = 0
-			end
-		end
+	end
+	if stamina == maxStamina then
+		timer = 0
 	end
 end)
 
 function setWalkMode(value)
- 	if value == "sprinting" then
+	if value == "sprinting" then
+		resetTimer()
 		player.WalkSpeed = moveSpeeds[3]
 	elseif value == "walking" then
 		player.WalkSpeed = moveSpeeds[2]
 	elseif value == "crouching" then
+		resetTimer()
 		player.WalkSpeed = moveSpeeds[1]
 	end
 	_G.moveMode = value
